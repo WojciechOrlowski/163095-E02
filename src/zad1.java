fe619dd28ba83c074de238c6ed52a8ee2649370f
WojciechOrlowski <wojtekorlowski741@wp.pl>
2022-02-14 14:22:50 +0100

    e02

--- src/zad1.java
+++ src/zad1.java
@@ -1,8 +1,9 @@
+
 public class zad1 {
     public static void main (String[]args){
         Integer[]int1 = new Integer[](1,2,1);
         Integer[]int2 = new Integer[](3,2,1);
-        System.out.println(jestPalindromem());
+        System.out.println(jestPalindromem(int1));
     }
 
     public static <T extends Comparable<T>> boolean jestPalindromem(T[] o){
